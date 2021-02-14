cc2b16aa631a608a5130eab5daf6b656d15e1412
yuuki-abc <yuki.abc.300@gmail.com>
2021-02-14 07:48:54 +0000

    ハッシュを使ってアプリを作成

--- ruby-book/lib/4.8.rb
+++ ruby-book/lib/4.8.rb
@@ -2,12 +2,12 @@
 
 # fruits.each_with_index { |fruits, i| puts "#{i}: #{fruits}" }
 
-dimensions = [
-  [10, 20],
-  [30, 40],
-  [50, 60]
-]
+# dimensions = [
+#   [10, 20],
+#   [30, 40],
+#   [50, 60]
+# ]
 
-dimensions.each_with_index do |(length, width), i|
-  puts "length: #{length}, width: #{width}, i: #{i}"
-end
+# dimensions.each_with_index do |(length, width), i|
+#   puts "length: #{length}, width: #{width}, i: #{i}"
+# end
