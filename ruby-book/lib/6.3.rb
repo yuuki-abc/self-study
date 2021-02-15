c29e059a4fba6d82dad909c5b512b71981b4caf8
yuuki-abc <yuki.abc.300@gmail.com>
2021-02-15 16:41:43 +0000

    ハッシュ記法の変換プログラムを作成、その準備が完了

--- ruby-book/lib/6.3.rb
+++ ruby-book/lib/6.3.rb
@@ -3,3 +3,18 @@
 # else
 #   puts 'マッチしませんでした'
 # end
+
+text = '私の誕生日は1997年7月17日です。'
+
+# m = /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/.match(text)
+if /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/ =~ text
+  puts "#{year}/#{month}/#{day}"
+end
+
+# a = m[:year]
+# b = m[:month]
+# c = m[:day]
+
+# puts a
+# puts b
+# puts c
